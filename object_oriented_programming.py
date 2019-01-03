548892e8b406411dce651979e73024c5161ae429
ayushmittal02 <ayushmittal02@gmail.com>
2019-01-03 13:32:00 +0530

    object_oriented_programming.py

---
+++ object_oriented_programming.py
@@ -0,0 +1,17 @@
+class Customer(object):
+
+	def __init__(self,name,balance=0.0):
+		self.name=name
+		self.balance=balance
+	def withdraw(self,amount):
+		if amount>self.balance:
+			raise RuntimeError('Amount greater than available balance')
+		self.balance-=amount
+		return self.balance
+	def deposit(self,amount):
+		self.balance+=amount
+		return self.balance
+
+ayush=Customer('Ayush Mittal',1000)
+ayush.withdraw(500)
+print(ayush.deposit(250))
